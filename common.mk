d80c3006a879556a79275dfeaa8ab36dcfb59488
firebird11 <hbgassel@gmail.com>
2018-12-16 12:29:00 +0100

    sdm845-common: add telephony-ext to boot-jars
    Change-Id: I6ac350c9036e0029ab6e3c98608d29831614babf
    Signed-off-by: varund7726 <date.varun123@gmail.com>

--- common.mk
+++ common.mk
@@ -116,6 +116,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# Spectrum
+PRODUCT_PACKAGES += \
+    init.spectrum.rc \
+    init.special_power.sh \
+    init.spectrum.sh
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext \
+    ims-ext-common \
+    qti-telephony-common
+
 # tri-state-key
 PRODUCT_PACKAGES += \
     KeyHandler \
